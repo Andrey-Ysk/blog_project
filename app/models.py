2fae4d161f31b53e98dd9c0d98501372303d8df1
Andrey.Y <and.yskaev@gmail.com>
2021-03-30 18:36:33 +0300

    add default new user role

--- app/models.py
+++ app/models.py
@@ -28,7 +28,7 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(64), unique=True, index=True)
     email = db.Column(db.String(64), unique=True)
     password_hash = db.Column(db.String(128))
-    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'), default=2)
     confirmed = db.Column(db.Boolean, default=False)
     posts = db.relationship('Post', backref='user')
     comments = db.relationship('Comment', backref='user')
